6f3c0b1ea56950da875a975c1bf8b425e1a8459f
ponciano.raf <ponciano.raf@nv99.com.br>
2022-09-03 00:30:07 -0300

    cobra init

---
+++ go-cli/cmd/random.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// randomCmd represents the random command
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "Meu comando randomzinho",
+	Long:  `Esse aqui é o help do meu comando randomzinho?.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("random called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// randomCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// randomCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
